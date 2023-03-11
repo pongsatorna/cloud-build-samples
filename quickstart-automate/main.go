b3653cd1dba2a9e6efa431697aa820c5458dd25b
pongsatorna <39115748+pongsatorna@users.noreply.github.com>
2023-03-11 18:36:30 +0700

    add main go

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+          package main
 
-import (
-"fmt"
-)
+          import (
+              "fmt"
+          )
 
-func main () {
-  fmt.Println("Hello, world!")
-}
+          func main() {
+              fmt.Println("Hello, universe!")
+          }
+   
